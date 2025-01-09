ceab4b1a430d3a110961814df15924d03f4aa207
Joël Luijmes <me@joell.dev>
2025-01-09 20:53:03 +0100

    feat: implement a card deck

--- bin/thousand_bombs_grenades.dart
+++ bin/thousand_bombs_grenades.dart
@@ -1,12 +1,18 @@
+import 'package:thousand_bombs_grenades/card_deck.dart';
 import 'package:thousand_bombs_grenades/die.dart';
 
 const seed = 42;
 
 void main(List<String> arguments) {
-  var dice = Die(seed);
-
   print("Throwing 10 times to illustrate our die works:");
+  var dice = Die(seed);
   for (var i = 0; i < 10; i++) {
     print(dice.roll());
+  }
+
+  print("Drawing 10 cards to illustrate our card deck works:");
+  var deck = CardDeck(seed);
+  for (var i = 0; i < 10; i++) {
+    print(deck.draw());
   }
 }
---
+++ lib/card_deck.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+/// Represents a card deck with following options:
+/// - Treasure Island: 🏝️💰
+/// - Skull: 💀
+/// - Watchman: 💂‍♂️⚔️
+/// - Pirate Ship: ⛵🏴‍☠️
+/// - Gold Coin: 🪙
+/// - Diamond: 💎
+/// - Animals: 🐒🦜
+class CardDeck {
+  final cards = [];
+
+  final Random _random;
+
+  CardDeck([int? seed]) : _random = Random(seed) {
+    _reshuffleDeck();
+  }
+
+  /// Draws a card from the deck.
+  String draw() {
+    if (cards.isEmpty) {
+      _reshuffleDeck();
+    }
+
+    return cards.removeAt(0);
+  }
+
+  /// Initializes the card deck with 10 cards for each type.
+  void _reshuffleDeck() {
+    // TODO: count how many cards actually exist for each type
+    for (var i = 0; i < 10; i++) {
+      cards.add("🏝️💰");
+      cards.add("💀");
+      cards.add("💂‍♂️⚔️");
+      cards.add("⛵🏴‍☠️");
+      cards.add("🪙");
+      cards.add("💎");
+      cards.add("🐒🦜");
+    }
+
+    cards.shuffle(_random);
+  }
+}
